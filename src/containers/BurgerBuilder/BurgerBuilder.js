e31b2b621ab9a9c2343f1c4768f9fd64a170e755
mohammadjhamayel3@gmail.com <mohammadjhamayel3@gmail.com>
2021-06-06 17:04:59 +0300

    First commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component{
+    render(){
+        return(
+            <Aux>
+                <Burger/>
+                <div>Burger Controls</div>
+                
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
