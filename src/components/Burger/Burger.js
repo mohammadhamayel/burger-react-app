a5e827b8495381dce9b252f1f562f02c9c6da8ac
mohammadjhamayel3@gmail.com <mohammadjhamayel3@gmail.com>
2021-06-07 13:03:38 +0300

    add dynamic ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,22 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    let transformedIngredient = Object.keys(props.ingredients)
+    .map(igKey => {
+        return [...Array(props.ingredients[igKey])].map(( _ , i ) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />;
+        });
+    }).reduce((arr,el) => { // reduce JS array function allow us transform array into another thing the 
+        // loop all elemnt and return them to the initial value 
+        return arr.concat(el);
+    },[]);
+    if(transformedIngredient.length ===0){
+        transformedIngredient = <p>please start adding Ingredients</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredient}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -6,7 +6,6 @@ import classes from './BurgerIngredient.module.css';
 class BurgerIngredient extends Component{
     render(){
         let ingredient = null;
-        console.log('this.props.type:   '+this.props.type);
         switch(this.props.type){
             case ('bread-bottom'):
                 ingredient = <div className={classes.BreadBottom}></div>;
