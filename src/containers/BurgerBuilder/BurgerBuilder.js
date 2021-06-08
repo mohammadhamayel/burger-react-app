46f74292c906b2bcbab81692124eec78dc72e516
mohammadjhamayel3@gmail.com <mohammadjhamayel3@gmail.com>
2021-06-08 15:59:36 +0300

    add order button and enable it when add ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,9 +21,18 @@ class BurgerBuilder extends Component{
             cheese:0,
             meat:0,
         },
-        totalPrice:4
+        totalPrice:4,
+        purchasable: false,
     }
 
+    updatePurchaseState (ingredients){
+        const sum = Object.keys(ingredients).map(igKey =>{
+            return ingredients[igKey];
+        }).reduce((sum,el) =>{
+            return sum + el;
+        },0);
+        this.setState({purchasable: sum > 0});
+    }
     addIngredientHandler= (type) => {
         const oldCount = this.state.ingredients[type];
         const updatedCount = oldCount +1 ;
@@ -33,6 +42,7 @@ class BurgerBuilder extends Component{
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
         this.setState({totalPrice:newPrice, ingredients:updatedIngredients});
+        this.updatePurchaseState(updatedIngredients);
     }
     removeIngredientHandler= (type) => {
         const oldCount = this.state.ingredients[type];
@@ -44,6 +54,7 @@ class BurgerBuilder extends Component{
             const oldPrice = this.state.totalPrice;
             const newPrice = oldPrice - priceDeduction;
             this.setState({totalPrice:newPrice, ingredients:updatedIngredients});
+            this.updatePurchaseState(updatedIngredients);
         }
     }
     
@@ -59,6 +70,7 @@ class BurgerBuilder extends Component{
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls ingredientAdded={this.addIngredientHandler} ingredientRemoved={this.removeIngredientHandler}
                 disabled = {disabledInfo}
+                purchasable={this.state.purchasable}
                 price={this.state.totalPrice}/>
                 
             </Aux>
