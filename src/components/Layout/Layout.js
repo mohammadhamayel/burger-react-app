69fa4c4ac2278b0a5bcbd3d02f091a6052c11f8a
mohammadjhamayel3@gmail.com <mohammadjhamayel3@gmail.com>
2021-06-10 10:30:47 +0300

    improve the app

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar></Toolbar>
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-    
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: false ,
+    }
+    sideDrawerCloseHandler =()=>{
+        this.setState({showSideDrawer: false});
+    }
 
-export default layout;
+    sideDrawerToggleHandle = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        });
+    }
+    render(){
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandle} />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+
+} 
+
+export default Layout;
